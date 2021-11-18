bea6c7d2f514dfc558baaed032b9d476f4b80ad2
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-18 17:48:39 +0530

    update query

--- mongodb.js
+++ mongodb.js
@@ -16,23 +16,54 @@ MongoClient.connect(
       return console.log("Unable to connect to databases!");
     }
     const db = client.db(databaseName);
-    db.collection("users").findOne(
-      { _id: new ObjectID("61913acc3f43687221d3e40a") },
-      (error, user) => {
-        if (error) {
-          return console.log("could not fetch the data");
-        }
-        console.log(user);
+  const updatePromise=  db.collection('users').updateOne({ _id: new ObjectID("6190779dfefea32f1f973fc2") }, {
+      $set: {
+    name:'Chinmay'
       }
-    );
-    db.collection("users")
-      .find({ age: 27 })
-      .toArray((error, user) => {
-        if (error) {
-          return console.log("could not find the match");
-        }
-        console.log(user);
-      });
+  })
+    updatePromise.then((res) => {
+      return console.log(res)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+);
+// db.collection("users").findOne(
+//   { _id: new ObjectID("61913acc3f43687221d3e40a") },
+//   (error, user) => {
+//     if (error) {
+//       return console.log("could not fetch the data");
+//     }
+//     console.log(user);
+//   }
+// );
+// db.collection("users")
+//   .find({ age: 27 })
+//   .toArray((error, user) => {
+//     if (error) {
+//       return console.log("could not find the match");
+//     }
+//     console.log(user);
+//   });
+
+
+// db.collection("tasks").findOne(
+//   { _id: new ObjectID("619078dd82e8e06daa3bbbf6") },
+//   (error, data) => {
+//     if (error) {
+//       return console.log("No doc found");
+//     }
+//     console.log(data);
+//   }
+// );
+// db.collection("tasks")
+//   .find({ completed: false })
+//   .toArray((error, data) => {
+//     if (error) {
+//       return console.log("No match found");
+//     }
+//     console.log(data);
+//   });
     // db.collection("users").insertOne(
     //   {
     //     _id: id,
@@ -83,23 +114,3 @@ MongoClient.connect(
     //     console.log(result);
     //   }
     // );
-
-    db.collection("tasks").findOne(
-      { _id: new ObjectID("619078dd82e8e06daa3bbbf6") },
-      (error, data) => {
-        if (error) {
-          return console.log("No doc found");
-        }
-        console.log(data);
-      }
-    );
-    db.collection("tasks")
-      .find({ completed: false })
-      .toArray((error, data) => {
-        if (error) {
-          return console.log("No match found");
-        }
-        console.log(data);
-      });
-  }
-);
