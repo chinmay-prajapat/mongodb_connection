8da090dd99ecdb0d678e8510b8ce468956105b93
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-18 18:03:23 +0530

    Refactor

--- mongodb.js
+++ mongodb.js
@@ -1,6 +1,8 @@
 // const mongodb = require("mongodb");
 // const MongoClient = mongodb.MongoClient;
 // const ObjectID = mongodb.ObjectId;
+
+
 const { MongoClient, ObjectID } = require("mongodb");
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
