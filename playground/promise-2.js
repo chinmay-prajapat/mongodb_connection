cae9f5232e0982d93a60b129db116592b41f29bc
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-21 14:12:24 +0530

    async/await

--- playground/promise-2.js
+++ playground/promise-2.js
@@ -1,14 +1,29 @@
 require("../src/db/mongoose")
 const { ObjectID } = require("bson")
+const { findByIdAndDelete } = require("../src/models/task")
 const Task = require("../src/models/task")
 
-Task.findByIdAndDelete({ _id: new ObjectID("6199ab965801d766bd96ad59") })
-  .then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-  })
-  .then((task) => {
-    console.log(task)
+// Task.findByIdAndDelete({ _id: new ObjectID("6199ab965801d766bd96ad59") })
+//   .then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+//   })
+//   .then((task) => {
+//     console.log(task)
+//   })
+//   .catch((e) => {
+//     console.log(e)
+//   })
+
+const deleteTask = async (id) => {
+  const deletedValue = await Task.findByIdAndDelete(id)
+  const countIncom = await Task.countDocuments({ completed: false })
+  return countIncom
+}
+
+deleteTask("61970eb87c691af655e7a486")
+  .then((count) => {
+    console.log(count)
   })
   .catch((e) => {
     console.log(e)
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,13 +1,25 @@
 require("../src/db/mongoose")
 const User = require("../src/models/user")
 // 61979091679832493c9125b0
-User.findByIdAndUpdate("61979091679832493c9125b0", { age: 10 })
-  .then((user) => {
-    console.log(user)
-    return User.countDocuments({ age: 0 })
-  })
-  .then((result) => {
-    console.log(result)
+// User.findByIdAndUpdate("61979091679832493c9125b0", { age: 10 })
+//   .then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 0 })
+//   })
+//   .then((result) => {
+//     console.log(result)
+//   })
+//   .catch((e) => {
+//     console.log(e)
+//   })
+const updateAgeAndCount = async (id, age) => {
+  const updateValue = await User.findByIdAndUpdate(id, { age })
+  const count = await User.countDocuments({ age })
+  return count
+}
+updateAgeAndCount("61979091679832493c9125b0", 15)
+  .then((count) => {
+    console.log(count)
   })
   .catch((e) => {
     console.log(e)
