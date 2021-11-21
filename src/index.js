3a8002874683dc4bca2e4085f3afe6b61b8e448e
chinmay-prajapat <prajapatchinmay@gmail.com>
2021-11-21 16:30:31 +0530

    making routes async

--- src/index.js
+++ src/index.js
@@ -7,19 +7,21 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-app.post("/users", (req, res) => {
+app.post("/users", async (req, res) => {
   const user = new User(req.body)
-  user
-    .save()
-    .then(() => {
-      res.status(201).send(user)
-    })
-    .catch((e) => {
-      res.status(400).send(e)
-    })
+  try {
+    await user.save()
+    res.status(201).send(user)
+  } catch (e) {
+    res.status(400).send(e)
+  }
 })
-app.get("/users", (req, res) => {
-  User.find({})
+app.get("/users",async (req, res) => {
+
+  try{
+  const user=User.find({})
+  }
+
     .then((users) => {
       res.send(users)
     })
